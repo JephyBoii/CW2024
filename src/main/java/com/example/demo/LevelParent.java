75d8c4b9651cb63bedbc41b4f849eb15fff3f3fd
Jep <renonomus@gmail.com>
2024-11-26 14:53:05 +0800

    UserPlane.java: - reworked velocity and multiplier variables - added moveRight() and moveLeft() functions LevelParent.java: -added key presses for additional userPlane movement

--- src/main/java/com/example/demo/LevelParent.java
+++ src/main/java/com/example/demo/LevelParent.java
@@ -108,13 +108,15 @@ public abstract class LevelParent extends Observable {
 				KeyCode kc = e.getCode();
 				if (kc == KeyCode.UP) user.moveUp();
 				if (kc == KeyCode.DOWN) user.moveDown();
+				if (kc == KeyCode.LEFT) user.moveLeft();
+				if (kc == KeyCode.RIGHT) user.moveRight();
 				if (kc == KeyCode.SPACE) fireProjectile();
 			}
 		});
 		background.setOnKeyReleased(new EventHandler<KeyEvent>() {
 			public void handle(KeyEvent e) {
 				KeyCode kc = e.getCode();
-				if (kc == KeyCode.UP || kc == KeyCode.DOWN) user.stop();
+				if (kc == KeyCode.UP || kc == KeyCode.DOWN || kc == KeyCode.LEFT || kc == KeyCode.RIGHT) user.stop();
 			}
 		});
 		root.getChildren().add(background);
--- src/main/java/com/example/demo/UserPlane.java
+++ src/main/java/com/example/demo/UserPlane.java
@@ -5,28 +5,37 @@ public class UserPlane extends FighterPlane {
 	private static final String IMAGE_NAME = "userplane.png";
 	private static final double Y_UPPER_BOUND = -40;
 	private static final double Y_LOWER_BOUND = 600.0;
+	private static final double X_UPPER_BOUND = 10000.0;
+	private static final double X_LOWER_BOUND = -10000.0;
 	private static final double INITIAL_X_POSITION = 5.0;
 	private static final double INITIAL_Y_POSITION = 300.0;
 	private static final int IMAGE_HEIGHT = 150;
-	private static final int VERTICAL_VELOCITY = 8;
+	private int VERTICAL_VELOCITY = 0;
+	private int HORIZONTAL_VELOCITY = 0;
 	private static final int PROJECTILE_X_POSITION = 110;
 	private static final int PROJECTILE_Y_POSITION_OFFSET = 20;
-	private int velocityMultiplier;
+	//private int velocityMultiplier;
 	private int numberOfKills;
 
 	public UserPlane(int initialHealth) {
 		super(IMAGE_NAME, IMAGE_HEIGHT, INITIAL_X_POSITION, INITIAL_Y_POSITION, initialHealth);
-		velocityMultiplier = 0;
+		//velocityMultiplier = 0;
 	}
 	
 	@Override
 	public void updatePosition() {
 		if (isMoving()) {
 			double initialTranslateY = getTranslateY();
-			this.moveVertically(VERTICAL_VELOCITY * velocityMultiplier);
-			double newPosition = getLayoutY() + getTranslateY();
-			if (newPosition < Y_UPPER_BOUND || newPosition > Y_LOWER_BOUND) {
+			double initialTranslateX = getTranslateX();
+			moveVertically(VERTICAL_VELOCITY );//* velocityMultiplier);
+			moveHorizontally(HORIZONTAL_VELOCITY);
+			double newPositionY = getLayoutY() + getTranslateY();
+			double newPositionX = getLayoutX() + getTranslateX();
+			if (newPositionY < Y_UPPER_BOUND || newPositionY > Y_LOWER_BOUND) {
 				this.setTranslateY(initialTranslateY);
+			}
+			if (newPositionX > X_UPPER_BOUND || newPositionX < X_LOWER_BOUND) {
+				this.setTranslateX(initialTranslateX);
 			}
 		}
 	}
@@ -42,19 +51,20 @@ public class UserPlane extends FighterPlane {
 	}
 
 	private boolean isMoving() {
-		return velocityMultiplier != 0;
+		return VERTICAL_VELOCITY != 0 || HORIZONTAL_VELOCITY != 0;
 	}
 
-	public void moveUp() {
-		velocityMultiplier = -1;
-	}
+	public void moveUp() { VERTICAL_VELOCITY = -8; }
 
-	public void moveDown() {
-		velocityMultiplier = 1;
-	}
+	public void moveDown() { VERTICAL_VELOCITY = 8; }
+
+	public void moveLeft() { HORIZONTAL_VELOCITY = -8; }
+
+	public void moveRight() { HORIZONTAL_VELOCITY = 8; }
 
 	public void stop() {
-		velocityMultiplier = 0;
+		VERTICAL_VELOCITY = 0;
+		HORIZONTAL_VELOCITY = 0;
 	}
 
 	public int getNumberOfKills() {
